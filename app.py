761e25dccd38baf033ef757f63627243cf79b39e
wangweimin <wang0.618@qq.com>
2021-09-22 21:47:46 +0800

    use websocket protocol by default

--- app.py
+++ app.py
@@ -45,10 +45,16 @@ def main():
 
 if __name__ == '__main__':
     import argparse
-    from pywebio.platform.tornado_http import start_server
+    from pywebio.platform.tornado_http import start_server as start_http_server
+    from pywebio import start_server as start_ws_server
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--port", type=int, default=8080)
+    parser.add_argument("--http", action="store_true", default=False, help='Whether to enable http protocol for communicates')
     args = parser.parse_args()
 
-    start_server(main, port=args.port)
+    if args.http:
+        start_http_server(main, port=args.port)
+    else:
+        start_ws_server(main, port=args.port)
+
